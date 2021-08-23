0cd2bb7611ac0e38db4ca0610353be6cc26fc44f
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2021-08-23 21:42:59 +0900

    Fixed the problems with id tags and added the submit button.  Next step is fix the if else statement so that if a range error occurs a message pops up.

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const AlphaNum = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890";
 const AlphaNumSpecChar = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890$!@&+?*/";
 
-var numOfChar = 12;
-
+var numOfChar = Number(document.getElementById("pwSize").value);
 var arr = [...Array(numOfChar).keys()];
 
+function updateNumOfChar(){
+    if(numOfChar < 27 && numOfChar > 0 && typeof(numOfChar) === "number"){
+        numOfChar = Number(document.getElementById("pwSize").value);
+        arr = [...Array(numOfChar).keys()];
+    }else{
+        numOfChar = 0;
+        arr = [...Array(numOfChar).keys()];
+    }
+    return arr;
+}
+
 function generateAlphaNum(){
-    //document.getElementById("password") =
-    return arr.map(num => num = AlphaNum[Math.floor(Math.random() * 61)]).join("");
+    document.getElementById("password").value = 
+    arr.map(num => num = AlphaNum[Math.floor(Math.random() * 61)]).join("");
 }
 
 function generateSpecChar(){
-    return arr.map((num, index) => index !== 0 ?
+    document.getElementById("specCharPassword").value =
+    arr.map((num, index) => index !== 0 ?
     num = AlphaNumSpecChar[Math.floor(Math.random() * 69)] :
     num = AlphaNum[Math.floor(Math.random() * 61)]).join("");
 }
 
-console.log(generateAlphaNum())
-console.log(generateSpecChar())
-console.log(document)
+console.log(document.getElementById("pwSize").value)
