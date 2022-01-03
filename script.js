d0c73c7819ac1b9e83946d766f14ebe34b403514
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-01-04 00:18:33 +0900

    I added the remove confusing characters feature but the code is very wet.  I will have to go over it and make it better. I also need to insert the copy feature.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const AlphaNum =
-  "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890";
 const AlphaNumSpecChar =
   "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890$!@%#^:+?*";
+const NoConfusingChar =
+  "aAbBcCdDeEfFgGhHijJkKLmMnNopPqQrRsStTuUvVwWxXyYzZ123456789$!@%#^:+?*";
 const SpecChar = "$!@%#^:+?*";
 const numOnly = "0123456789";
 
@@ -33,34 +33,47 @@ function generateNum() {
     .join("");
 }
 
-function generateAlphaNum() {
+function generateAlphaNum(str) {
   updateNumOfChar();
+
+  document.getElementById("checkBox").checked ? 
+  str = NoConfusingChar :
+  str = AlphaNumSpecChar;
+
   document.getElementById("password").value = arr
-    .map((num) => (num = AlphaNum[Math.floor(Math.random() * 62)]))
+    .map((num) => (num = str[Math.floor(Math.random() * 62)]))
     .join("");
 }
 
 //This function just insures at least one number is in the Special Characters Password
 function insertNum(string) {
-  return string.match(/\d/) == null &&
-  string.length > 2
-  ? string.replace(string[string.length - 2], numOnly[Math.floor(Math.random() * 10)]) : string;
+  return string.match(/\d/) == null && string.length > 2
+    ? string.replace(
+        string[string.length - 2],
+        numOnly[Math.floor(Math.random() * 10)]
+      )
+    : string;
 }
 
-function generateSpecChar() {
+function generateSpecChar(str) {
+  
   updateNumOfChar();
 
+  document.getElementById("checkBox").checked ? 
+  str = NoConfusingChar :
+  str = AlphaNumSpecChar;
+  
   let string = arr
-  .map((num, index) => 
-  index + 1 === arr.length
-  ? (num = SpecChar[Math.floor(Math.random() * 10)])
-  : index !== 0
-  ? (num = AlphaNumSpecChar[Math.floor(Math.random() * 72)])
-  : (num = AlphaNum[Math.floor(Math.random() * 52)])
-  )
-  .join("");
-  
-  insertNum(string)
-  
+    .map((num, index) =>
+      index + 1 === arr.length
+        ? (num = SpecChar[Math.floor(Math.random() * 10)])
+        : index !== 0
+        ? (num = str[Math.floor(Math.random() * 72)])
+        : (num = str[Math.floor(Math.random() * 52)])
+    )
+    .join("");
+
+  insertNum(string);
+
   document.getElementById("specCharPassword").value = string;
 }
